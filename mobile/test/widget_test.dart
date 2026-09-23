16358eff592bd948953a2f78407bb90b1b0771ee
m01270114218m-code <m01270114218m@gmail.com>
2026-09-23 08:43:44 +0300

    Fix widget test for embedded design screen

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mt2_voice_party/main.dart';
+import 'package:mt2_voice_party/screens/myninja_design_screen.dart';
 
 void main() {
-  testWidgets('MT2 app starts on the home screen', (tester) async {
+  testWidgets('MT2 app starts with the embedded design screen', (tester) async {
     await tester.pumpWidget(const Mt2App());
-    expect(find.text('MT2 PARTY'), findsOneWidget);
-    expect(find.text('Popular Rooms'), findsOneWidget);
+    expect(find.byType(Mt2DesignScreen), findsOneWidget);
   });
 }
