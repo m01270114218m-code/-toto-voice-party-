f5da21f3203119f9d26e9ae48724836318fb3d61
m01270114218m-code <m01270114218m@gmail.com>
2026-09-23 11:57:47 +0300

    Initialize Royal Voice LiveKit project

---
+++ public/mobile-config.js
@@ -0,0 +1,3 @@
+window.ROYAL_API_BASE=window.ROYAL_API_BASE||'';
+window.ROYAL_SUPABASE_URL='https://hgsfdkopbbwbtvsrbpoi.supabase.co';
+window.ROYAL_SUPABASE_PUBLISHABLE_KEY='sb_publishable_-4jjp6J2GLVPkv8AW95TZQ_DLOEIWOm';
