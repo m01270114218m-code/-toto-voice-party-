9a98a189d78418280d58b63507d7a9c5f390e285
m01270114218m-code <m01270114218m@gmail.com>
2026-09-24 08:11:27 +0300

    Remove legacy mobile web configuration

--- public/mobile-config.js
+++
@@ -1,4 +0,0 @@
-// Android/Capacitor runtime configuration.
-window.ROYAL_API_BASE = window.ROYAL_API_BASE || '';
-window.ROYAL_SUPABASE_URL = 'https://hgsfdkopbbwbtvsrbpoi.supabase.co';
-window.ROYAL_SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_-4jjp6J2GLVPkv8AW95TZQ_DLOEIWOm';
