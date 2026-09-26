06e7b7a2501f41215c16a81447401c42c5883abf
m01270114218m-code <m01270114218m@gmail.com>
2026-09-26 04:08:26 +0300

    Remove legacy project file

--- capacitor.config.ts
+++
@@ -1,8 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = {
-  appId: 'com.toto.voiceparty',
-  appName: 'Toto Voice Party',
-  webDir: 'dist',
-  bundledWebRuntime: false,
-};
-export default config;
